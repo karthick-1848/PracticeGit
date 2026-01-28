077158a7ed579583ade8ff524a2409198eb93490
karthick-1848 <vmrkarthick0510@gmail.com>
2026-01-28 10:20:36 +0530

    Initial commit

---
+++ src/main/java/org/example/ClassA.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class ClassA
+{
+    String name;
+    int age;
+    String department;
+    int salary;
+
+    ClassA()
+    {
+        System.out.println("Class A constructor is created");
+    }
+}
