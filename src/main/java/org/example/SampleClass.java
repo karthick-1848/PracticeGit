f5bb7ae8705c59d25eb2d513d7fd41f6f690b198
karthick-1848 <vmrkarthick0510@gmail.com>
2026-01-28 17:42:32 +0530

    Feature branch is again running

--- src/main/java/org/example/SampleClass.java
+++ src/main/java/org/example/SampleClass.java
@@ -7,5 +7,6 @@ public class SampleClass
         System.out.println("Sample Class Created");
         System.out.println("Feature One branch is running now");
         System.out.println("Feature One branch is again started");
+        System.out.println("Feature One branch is again started");
     }
 }
