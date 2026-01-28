0bd7f29dee840c6414a85412a862e8686179715e
karthick-1848 <vmrkarthick0510@gmail.com>
2026-01-28 12:52:27 +0530

    Feature branch sample class added

--- src/main/java/org/example/SampleClass.java
+++ src/main/java/org/example/SampleClass.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class SampleClass
 {
-    static void main() {
+    static void main()
+    {
         System.out.println("Sample Class Created");
+        System.out.println("Feature One branch is running now");
     }
 }
