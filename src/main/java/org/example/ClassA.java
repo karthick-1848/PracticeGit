5fd017d719ae6bcc4c14004e8eceb736ca1c43fa
karthick-1848 <vmrkarthick0510@gmail.com>
2026-01-29 15:27:08 +0530

    local changes

--- src/main/java/org/example/ClassA.java
+++ src/main/java/org/example/ClassA.java
@@ -13,5 +13,6 @@ public class ClassA
         System.out.println("Class A constructor is created 2nd time");
         System.out.println("Class A constructor is created 3rd time");
         System.out.println("Hi this is qwertyuiop");
+        System.out.println("This is Chennai");
     }
 }
